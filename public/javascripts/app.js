ee79c305884cc5d486c6e8fd9954ff5fd4926f2d
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-04 12:17:27 -0600

    refactor app.js to correct location input values

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,5 @@
 // geolocation functions for new and edit forms 
-const locationForm = document.getElementById('add-location-form');
+const locationForm = document.getElementById('trip-details-form');
 const currentRadio = document.getElementById('current-radio');
 const customRadio = document.getElementById('custom-radio');
 const skipRadio = document.getElementById('skip-radio');
@@ -9,22 +9,27 @@ const deleteLink = document.getElementById('delete-link');
 const deleteVerification = document.getElementById('delete-verification');
 const noEndDateEl = document.getElementById("end-date-checkbox");
 const endDateInputEl = document.getElementById("trip-end-date");
+const tripLat = document.getElementById('lat-coords').innerText;
+const tripLong = document.getElementById('long-coords').innerText;
 let currentLat;
 let currentLong;
 navigator.geolocation.getCurrentPosition(function(location) {
     currentLat = location.coords.latitude;
     currentLong = location.coords.longitude;
-    locationInput.setAttribute('value', `${currentLat}, ${currentLong}`);
+    if(currentRadio.checked) {
+        locationInput.setAttribute('value', `${currentLat}, ${currentLong}`);
+    };
 });
 
 locationForm.addEventListener('click', function(e) {
     if (currentRadio.checked || skipRadio.checked) {
         locationDivEl.setAttribute('style', 'display: none');
-        locationInput.setAttribute('value', `${currentLat}, ${currentLong}`);
-    } else {
+        if(currentRadio.checked) {
+            locationInput.setAttribute('value', `${currentLat}, ${currentLong}`);
+        };
+    } else if(customRadio.checked) {
+        locationInput.setAttribute('value', `${tripLat}, ${tripLong}`);
         locationDivEl.setAttribute('style', 'display: block');
-        locationInput.setAttribute('value', '');
-        locationInput.setAttribute('placeholder', 'lat, long');
     };
     if (noEndDateEl.checked) {
         endDateInputEl.value = null; 
